e66e703a418b4f04a659273b16cb75f5e81555e7
Jakub Folta <jakubfolta@yahoo.co.uk>
2018-11-13 20:47:19 +0100

    finished

--- count.py
+++ count.py
@@ -1,2 +1,9 @@
 def count(sequence, item):
-  
+  total = 0
+  for index in sequence:
+    if index == item:
+      total += 1
+  return total
+
+print(count(['ab', 'dr', 'd', 'ab', 'ut', 'aba', 'd', 'ab'], 'ab'))
+    
