d4bdd8342665b9c7ca9c01a976246ec27ab7f9fe
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-03-25 12:16:16 +0100

    continuation

--- count_items.py
+++ count_items.py
@@ -2,3 +2,15 @@ def count_item(sequence, item_to_count):
     return sequence.count(item_to_count)
 
 print(count_item(['dada', 'dsadef', 'das', 'dade', 'asa', 'fsdf', 'asa'], 'asa'))
+
+def count_items(sequence, item):
+    total = 0
+    for x in sequence:
+        if x == item:
+            total += 1
+    return total
+
+print(count_items(['dada', 'dsadef', 'das', 'dade', 'asa', 'fsdf', 'asa'], 'asa'))
+
+def count_items(sequence, item):
+    
